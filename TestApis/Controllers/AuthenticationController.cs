58f527599edefb5a0549a988fb7b3da679d65a79
Umer Sultan <umer.sultan@vadion.com>
2021-03-17 20:30:39 +0500

    Add project files.

---
+++ TestApis/Controllers/AuthenticationController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using TestApis.Database;
+using TestApis.ViewModels;
+
+namespace TestApis.Controllers
+{
+   // [Authorize]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private RoleManager<IdentityRole> roleManager;
+        private readonly IConfiguration configuration;
+
+        public AuthenticationController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+            this.configuration = configuration;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] RegisterViewModel registerViewModel)
+        {
+            var userExist = await userManager.FindByNameAsync(registerViewModel.UserName);
+            if (userExist != null)
+                return BadRequest("User is already exist");
+            var AppUser = new ApplicationUser()
+            {
+                UserName = registerViewModel.UserName,
+                Email = registerViewModel.Email,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+            var result = await userManager.CreateAsync(AppUser, registerViewModel.Password);
+            if (!result.Succeeded)
+                return BadRequest("user creation failed");
+
+            return Ok("User Successfully Created");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginViewModel loginViewModel)
+        {
+            var userExist = await userManager.FindByNameAsync(loginViewModel.UserName);
+            if (userExist != null && await userManager.CheckPasswordAsync(userExist, loginViewModel.Password))
+            {
+                var userRoles = await userManager.GetRolesAsync(userExist);
+                var authClaims = new List<Claim>{
+                new Claim(ClaimTypes.Name , userExist.UserName),
+                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+                };
+                foreach(var role in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, role));
+                }
+                var signKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetValue<string>("JWT:Key")));
+
+                var token = new JwtSecurityToken(
+                    issuer: configuration.GetValue<string>("JWT:ValidIssuer"),
+                    audience: configuration.GetValue<string>("JWT:ValidAudience"),
+                    expires: DateTime.Now.AddHours(5),
+                    claims: authClaims,
+                    signingCredentials: new SigningCredentials(signKey,SecurityAlgorithms.HmacSha256)
+                    );
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+            }
+
+            return Unauthorized();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("Get Request Successful");
+        }
+    }
+}
---
+++ TestApis/Startup.cs
@@ -0,0 +1,101 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestApis.Database;
+
+namespace TestApis
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //AppDbContex
+            services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(Configuration.GetConnectionString("ConString")));
+            ///identity
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options  => {
+                    options.SaveToken = true;
+                    options.RequireHttpsMetadata = false;
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidAudience = Configuration["JWT:ValidAudience"],
+                        ValidIssuer = Configuration["JWT:ValidIssuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Key"]))
+
+                    };
+
+                    });
+
+            services.AddCors(options => options.AddPolicy("Cors", builder =>
+            {
+                builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+            }));
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TestApis", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,ApplicationDbContext context)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TestApis v1"));
+            }
+            app.UseCors("Cors");
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            context.Database.Migrate();
+        }
+    }
+}
